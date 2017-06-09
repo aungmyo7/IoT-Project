f051e482d31245f4a7d0f98add7b48e4b1db3570
Nyi Nyi Zin <nyinyizin@users.noreply.github.com>
2017-06-09 11:56:24 +0800

    Updated for static folder

--- SmartAgriWeb/SmartAgriWeb/settings.py
+++ SmartAgriWeb/SmartAgriWeb/settings.py
@@ -83,5 +83,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
